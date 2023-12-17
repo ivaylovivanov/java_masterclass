e223f4c19dc647a82e2a03dc32a568d51d3d396f
Ivaylo Ivanov <ivalentinov@gmail.com>
2023-12-18 00:49:10 +0200

    Merge pull request #2
    * JM chapter 15 - additional changes to hello world. adding a variable

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Tim");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 10;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 1000;
+        System.out.println(myFirstNumber);
+
+        myFirstNumber = 10 + 5;
+        System.out.println(myFirstNumber);
     }
+
 }
