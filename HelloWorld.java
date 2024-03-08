b7d0d6bffc2646df845e57ca80aa9c2b2c0d7d10
ivaylovivanov <ivalentinov@gmail.com>
2024-03-08 16:03:57 +0200

    JM chapter 18 - checking out byte, short and long

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,6 +11,22 @@ public class HelloWorld {
 
         System.out.println("Integer value range is (" + myMinIntValue + " to " + myMaxIntValue + ")");
 
+        System.out.println("Byte value range is (" + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE + ")");
+
+        System.out.println("Short value range is (" + Short.MIN_VALUE + " to " + Short.MAX_VALUE + ")");
+
+        System.out.println("Long value range is (" + Long.MIN_VALUE + " to " + Long.MAX_VALUE + ")");
+        System.out.println("Long has a width of " + Long.SIZE);
+
+        long myLongValue = 100;
+        System.out.println(myLongValue);
+
+        long bigLongLiteralValue = 2_247_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+        System.out.println(bigShortLiteralValue);
+
     }
 
 }
