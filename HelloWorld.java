145fc094642dd13a20ef5c9fab816ded0c6370ec
ivaylovivanov <ivalentinov@gmail.com>
2024-03-08 16:04:15 +0200

    JM chapter 18 - checking out byte, short and long

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int myValue = 10000;
-
         int myMinIntValue = Integer.MIN_VALUE;
         System.out.println(myMinIntValue);
 
