78fd25cf5097a1aa3ba7c3569d24f413c3cd9785
ivaylovivanov <ivalentinov@gmail.com>
2024-03-10 19:34:22 +0200

    JM chapter 19 - detailed look at byte, short and int

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,18 @@ public class HelloWorld {
     public static void main(String[] args) {
         short myMinShortValue = Short.MIN_VALUE;
         int myMinIntValue = Integer.MIN_VALUE;
-        byte myByteMinValue = Byte.MIN_VALUE, myByteMaxValue = Byte.MAX_VALUE;
+        byte myByteMinValue = Byte.MIN_VALUE;
         short firstShort = 1;
         int firstInt = 2;
 
         int myTotal = (myMinIntValue / 2);
+        System.out.println(myTotal);
 
         byte myNewByteValue = (byte) (myByteMinValue / 2);
+        System.out.println(myNewByteValue);
 
         short myNewShortValue = (short) (myMinShortValue / 2);
+        System.out.println(myNewShortValue);
     }
 
 }
