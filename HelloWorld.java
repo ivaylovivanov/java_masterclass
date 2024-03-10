9357ed45612dcf7f2dcee128ac8610a7e3b5450a
ivaylovivanov <ivalentinov@gmail.com>
2024-03-10 19:32:35 +0200

    JM chapter 19 - detailed look at byte, short and int

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,30 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        short myMinShortValue = Short.MIN_VALUE;
         int myMinIntValue = Integer.MIN_VALUE;
-        System.out.println(myMinIntValue);
+        byte myByteMinValue = Byte.MIN_VALUE, myByteMaxValue = Byte.MAX_VALUE;
+        short firstShort = 1;
+        int firstInt = 2;
 
-        int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer Maximum value is " + myMaxIntValue);
-        System.out.println("Integer Maximum value is " + Integer.MAX_VALUE);
+        int myTotal = (myMinIntValue / 2);
 
-        System.out.println("Integer value range is (" + myMinIntValue + " to " + myMaxIntValue + ")");
+        byte myNewByteValue = (byte) (myByteMinValue / 2);
 
-        System.out.println("Byte value range is (" + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE + ")");
-
-        System.out.println("Short value range is (" + Short.MIN_VALUE + " to " + Short.MAX_VALUE + ")");
-
-        System.out.println("Long value range is (" + Long.MIN_VALUE + " to " + Long.MAX_VALUE + ")");
-        System.out.println("Long has a width of " + Long.SIZE);
-
-        long myLongValue = 100;
-        System.out.println(myLongValue);
-
-        long bigLongLiteralValue = 2_247_483_647_234L;
-        System.out.println(bigLongLiteralValue);
-
-        short bigShortLiteralValue = 32767;
-        System.out.println(bigShortLiteralValue);
-
+        short myNewShortValue = (short) (myMinShortValue / 2);
     }
 
 }
