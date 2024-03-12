b6790378ff4653e533d83010cdb6429444931eaf
ivaylovivanov <ivalentinov@gmail.com>
2024-03-12 18:00:29 +0200

    JM chapter 21 - primitive types challenge

---
+++ JM21.java
@@ -0,0 +1,22 @@
+public class JM21 {
+    public static void main(String[] args) {
+        System.out.println("Float value range " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+
+        System.out.println("Double value range " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+
+        int myIntValue = 5;
+        float myFloatValue = 5;
+        double myDoubleValue = 5;
+
+        System.out.println("int: " + myIntValue);
+        System.out.println("float: " + myFloatValue);
+        System.out.println("double: " + myDoubleValue);
+
+        System.out.println("float: " + myFloatValue);
+        System.out.println("double: " + myDoubleValue);
+
+        float myOtherFloatValue = 5.25f;
+        System.out.println(myOtherFloatValue);
+
+    }
+}
