321d6f7273fe08621f5c858079c9e5fdb08483ba
ivaylovivanov <ivalentinov@gmail.com>
2024-04-02 00:33:29 +0300

    JM chapter 36 - OR operator

--- HelloWorld.java
+++ HelloWorld.java
@@ -18,5 +18,10 @@ public class HelloWorld {
         if (topScore > secondTopScore && topScore <= 100){
             System.out.println("Greater than second top score and less than 100");
         }
+
+        if (topScore > 90 || secondTopScore <= 90){
+            System.out.println("Either, or both of the conditions are true.");
+        }
+
     }
 }
