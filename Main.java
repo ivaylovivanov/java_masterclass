d66be49ebdb8abf0dad2d64b5d36bcf1ebc12511
ivaylovivanov <ivalentinov@gmail.com>
2024-05-15 14:30:15 +0300

    CE 04 Barking Dog

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        //CE-02 - Speed Converter
-        SpeedConverter.printConversion(1.5);
-        SpeedConverter.printConversion(10.25);
-        SpeedConverter.printConversion(-5.6);
-        SpeedConverter.printConversion(25.42);
-        SpeedConverter.printConversion(75.114);
+//        //CE-02 - Speed Converter
+//        SpeedConverter.printConversion(1.5);
+//        SpeedConverter.printConversion(10.25);
+//        SpeedConverter.printConversion(-5.6);
+//        SpeedConverter.printConversion(25.42);
+//        SpeedConverter.printConversion(75.114);
+//
+//        //CE-03 - MegaBytesConverter
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
 
-        //CE-03 - MegaBytesConverter
-        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+        //CE-04
+        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+        System.out.println(BarkingDog.shouldWakeUp(false, 2));
+        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+        System.out.println(BarkingDog.shouldWakeUp(true, -1));
 
 
     }
