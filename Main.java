404a2712538f894c6f1fc5c36f4648321f0aaac4
ivaylovivanov <ivalentinov@gmail.com>
2024-05-15 15:17:43 +0300

    CE 05 Leap Year

--- Main.java
+++ Main.java
@@ -13,11 +13,24 @@ public class Main {
 //        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
 //        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
 
-        //CE-04
-        System.out.println(BarkingDog.shouldWakeUp(true, 1));
-        System.out.println(BarkingDog.shouldWakeUp(false, 2));
-        System.out.println(BarkingDog.shouldWakeUp(true, 8));
-        System.out.println(BarkingDog.shouldWakeUp(true, -1));
+//        //CE-04
+//        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+//        System.out.println(BarkingDog.shouldWakeUp(false, 2));
+//        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+//        System.out.println(BarkingDog.shouldWakeUp(true, -1));
+
+        System.out.println("1600 is " + (LeapYear.isLeapYear(1600) ? "leap year" : "not a leap year"));
+        System.out.println("1700 is " + (LeapYear.isLeapYear(1700) ? "leap year" : "not a leap year"));
+        System.out.println("1800 is " + (LeapYear.isLeapYear(1800) ? "leap year" : "not a leap year"));
+        System.out.println("1900 is " + (LeapYear.isLeapYear(1900) ? "leap year" : "not a leap year"));
+        System.out.println("2000 is " + (LeapYear.isLeapYear(2000) ? "leap year" : "not a leap year"));
+        System.out.println("2017 is " + (LeapYear.isLeapYear(2017) ? "leap year" : "not a leap year"));
+        System.out.println("2100 is " + (LeapYear.isLeapYear(2100) ? "leap year" : "not a leap year"));
+        System.out.println("2200 is " + (LeapYear.isLeapYear(2200) ? "leap year" : "not a leap year"));
+        System.out.println("2300 is " + (LeapYear.isLeapYear(2300) ? "leap year" : "not a leap year"));
+        System.out.println("2400 is " + (LeapYear.isLeapYear(2400) ? "leap year" : "not a leap year"));
+        System.out.println("2500 is " + (LeapYear.isLeapYear(2500) ? "leap year" : "not a leap year"));
+        System.out.println("2600 is " + (LeapYear.isLeapYear(2600) ? "leap year" : "not a leap year"));
 
 
     }
