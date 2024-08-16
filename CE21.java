373bae10df73ce11c2626c49574c41c74b09e9f9
ivaylovivanov <ivalentinov@gmail.com>
2024-08-16 11:15:42 +0300

    CE-21 printFactors Printing the factors of a given number

---
+++ CE21.java
@@ -0,0 +1,24 @@
+public class CE21 {
+    public static void main(String[] args) {
+        printFactors(6);
+        System.out.println();
+        printFactors(32);
+        System.out.println();
+        printFactors(10);
+        System.out.println();
+        printFactors(-1);
+    }
+
+    public static void printFactors(int number){
+        if(number < 1){
+            System.out.println("Invalid Value");
+        } else {
+            for(int i = 1; i <= number; i++){
+                if(number % i == 0){
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
+    }
+}
