e6c0cca38a49bc44e802bc72a9e4be8244973e38
ivaylovivanov <ivalentinov@gmail.com>
2024-08-21 23:43:35 +0300

    CE-29 getBucketCount calculating the number of buckets of paint needed for the given area

---
+++ CE/src/CE29.java
@@ -0,0 +1,40 @@
+public class CE29 {
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(areaPerBucket <= 0 || width <= 0 || height <= 0 || extraBuckets < 0){
+            return -1;
+        }
+
+        double area = width * height;
+        double bucketsNeeded = area / areaPerBucket;
+
+        return (int) Math.ceil(bucketsNeeded) - extraBuckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        return getBucketCount(width, height, areaPerBucket, 0);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(areaPerBucket <= 0 || area <= 0){
+            return -1;
+        }
+
+        double bucketsNeeded = area / areaPerBucket;
+
+        return (int) Math.ceil(bucketsNeeded);
+    }
+}
