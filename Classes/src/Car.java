df7339c22d2f44800617e2656a87027c55849f9a
ivaylovivanov <ivalentinov@gmail.com>
2024-08-23 00:03:19 +0300

    JM-79 OOP creating an Account class

--- Classes/src/Car.java
+++ Classes/src/Car.java
@@ -1,16 +1,61 @@
 public class Car {
 
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
+    private String make = "Mazda";
+    private String model = "CX-80";
+    private String color = "Artisan Red";
+    private int doors = 5;
     private boolean convertible;
 
     public void describeCar(){
-        System.out.println(doors + "-Doors" +
+        System.out.println(doors + "-Door " +
                 color + " " +
                 make + " " +
                 model + " " +
                 (convertible ? "Convertible" : ""));
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setMake(String make) {
+        if(make == null) make = "Unknown";
+        String lowerCaseMake = make.toLowerCase();
+        switch (lowerCaseMake){
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> this.make = "Unsupported";
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
     }
 }
--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Account myAccount = new Account();
+        myAccount.setBalance(0.0);
+        myAccount.setEmail("ivalentinov@gmail.com");
+        myAccount.setName("Ivaylo Ivanov");
+        myAccount.setNumber(1);
+        myAccount.setPhoneNumber("+359887424989");
+
+        System.out.println("Account Created: " +
+                myAccount.getName() + " " +
+                myAccount.getBalance() + " " +
+                myAccount.getEmail() + " " +
+                myAccount.getPhoneNumber() + " " +
+                myAccount.getNumber());
+
+        myAccount.depositFunds(110.11);
+        System.out.println("Account Deposit: " +
+                myAccount.getName() + " " +
+                myAccount.getBalance() + " " +
+                myAccount.getEmail() + " " +
+                myAccount.getPhoneNumber() + " " +
+                myAccount.getNumber());
+
+        myAccount.withdrawFunds(22);
+        System.out.println("Account Withdrawal: " +
+                myAccount.getName() + " " +
+                myAccount.getBalance() + " " +
+                myAccount.getEmail() + " " +
+                myAccount.getPhoneNumber() + " " +
+                myAccount.getNumber());
+
+
+
+
     }
 }
