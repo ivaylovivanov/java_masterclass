f6357ccd3cebeb0b76728f7ed12b2884d1e54a8f
ivaylovivanov <ivalentinov@gmail.com>
2024-08-24 00:16:24 +0300

    CE-33 Calculating the distance between two points

--- CE/src/Main.java
+++ CE/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        CE32Wall wall = new CE32Wall(5,4);
-        System.out.println("area= " + wall.getArea());
-        wall.setHeight(-1.5);
-        System.out.println("width= " + wall.getWidth());
-        System.out.println("height= " + wall.getHeight());
-        System.out.println("area= " + wall.getArea());
+        CE33Point first = new CE33Point(6, 5);
+        CE33Point second = new CE33Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        CE33Point point = new CE33Point();
+        System.out.println("distance()= " + point.distance());
     }
 }
