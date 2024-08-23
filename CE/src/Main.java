9b2bf363e8047e4cc1b1e7d437f4eb5e61b7ac59
ivaylovivanov <ivalentinov@gmail.com>
2024-08-23 11:17:02 +0300

    CE-31 simple Person class that calculates if a person is a teenager and returns the full name

--- CE/src/Main.java
+++ CE/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        /*
         CE30SimpleCalculator calculator = new CE30SimpleCalculator();
         calculator.setFirstNumber(5.0);
         calculator.setSecondNumber(4);
@@ -10,6 +10,19 @@ public class Main {
         calculator.setSecondNumber(0);
         System.out.println("multiply= " + calculator.getMultiplicationResult());
         System.out.println("divide= " + calculator.getDivisionResult());
+        */
+        CE31Person person = new CE31Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
 
     }
 }
