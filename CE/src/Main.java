fb1c2450e6bee2d453f1f2c6e6344cd27323de3d
ivaylovivanov <ivalentinov@gmail.com>
2024-08-23 23:26:30 +0300

    CE-32 Wall Class with constructors

--- CE/src/Main.java
+++ CE/src/Main.java
@@ -1,28 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        CE30SimpleCalculator calculator = new CE30SimpleCalculator();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add= " + calculator.getAdditionResult());
-        System.out.println("subtract= " + calculator.getSubtractionResult());
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply= " + calculator.getMultiplicationResult());
-        System.out.println("divide= " + calculator.getDivisionResult());
-        */
-        CE31Person person = new CE31Person();
-        person.setFirstName("");   // firstName is set to empty string
-        person.setLastName("");    // lastName is set to empty string
-        person.setAge(10);
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-        person.setFirstName("John");    // firstName is set to John
-        person.setAge(18);
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-        person.setLastName("Smith");    // lastName is set to Smith
-        System.out.println("fullName= " + person.getFullName());
-
+        CE32Wall wall = new CE32Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
