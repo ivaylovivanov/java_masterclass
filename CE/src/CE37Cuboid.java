d9fed3db68a84928f4fc9d3a16f38c44572bf2f8
ivaylovivanov <ivalentinov@gmail.com>
2024-08-31 00:13:48 +0300

    CE-37 Rectangle and Cuboid

---
+++ CE/src/CE37Cuboid.java
@@ -0,0 +1,17 @@
+public class CE37Cuboid extends CE37Rectangle {
+    private double height;
+
+    public CE37Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return getArea() * height;
+    }
+
+}
