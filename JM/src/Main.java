d55b1200c2ad7e54edf26866b224fa1cb9eaa38e
ivaylovivanov <ivalentinov@gmail.com>
2024-09-03 18:04:25 +0300

    JM-105 Encapsulation Basics

--- JM/src/Main.java
+++ JM/src/Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        JM104Player player = new JM104Player();
-        player.fullName  = "Ivo";
-        player.health = 20;
-        player.weapon = "Sword";
-
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health: " + player.health);
-        player.health = 200;
-        player.loseHealth(damage);
-        System.out.println("Remaining health: " + player.health);
+        JM105EnhancedPlayer ivo = new JM105EnhancedPlayer("Ivaylo Ivanov", 200, "Sword");
+        System.out.println("Initial health is " + ivo.healthRemaining());
     }
 }
