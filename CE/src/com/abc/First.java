91dfe0f473e4d6b118c4083acacb3990cc68f1fc
ivaylovivanov <ivalentinov@gmail.com>
2024-09-11 23:34:14 +0300

    JM-116 packages

---
+++ CE/src/com/abc/First.java
@@ -0,0 +1,16 @@
+package com.abc;
+
+public class First {
+    private String type;
+
+    public First(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "First{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
---
+++ CE/src/dev/lpa/Main.java
@@ -0,0 +1,10 @@
+package dev.lpa;
+
+import com.abc.*;
+
+public class Main {
+    public static void main(String[] args) {
+        First item = new First("Burger");
+        System.out.println(item);
+    }
+}
